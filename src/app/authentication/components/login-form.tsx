a35e2165d6e6175ed9cb15d3f143f402e09fe5e1
Allan Francis <allantecnologo@gmail.com>
2025-05-31 09:00:29 -0400

    feat: Adiciona componentes de formulário para autenticação com validação utilizando Zod e React Hook Form

---
+++ src/app/authentication/components/login-form.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { FormLabel, FormMessage } from "@/components/ui/form";
+import { FormField } from "@/components/ui/form";
+import { FormControl } from "@/components/ui/form";
+import { Form, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const registerSchema = z.object({
+  name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
+  email: z.string().trim().min(1, { message: "Email é obrigatório" }).email({
+    message: "Email inválido",
+  }),
+  password: z
+    .string()
+    .trim()
+    .min(6, { message: "Senha deve ter pelo menos 6 caracteres" }),
+});
+
+const SignUpForm = () => {
+  const formRegister = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof registerSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Card>
+      <Form {...formRegister}>
+        <form
+          onSubmit={formRegister.handleSubmit(onSubmit)}
+          className="space-y-4"
+        >
+          <CardHeader>
+            <CardTitle>Entrar</CardTitle>
+            <CardDescription>Faça o login para continuar</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <FormField
+              control={formRegister.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={formRegister.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Criar conta</Button>
+          </CardFooter>
+        </form>
+      </Form>
+    </Card>
+  );
+};
+
+export default SignUpForm;
