d2d8b7dea4c3f15e812d35c4927e2cbb00749088
Allan Francis <allantecnologo@gmail.com>
2025-05-31 09:00:21 -0400

    feat: Adiciona página de autenticação com formulários de login e registro utilizando componentes de UI de abas

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,28 @@
+import {} from "@/components/ui/form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import LoginForm from "./components/login-form";
+import SignUpForm from "./components/sign-up-form";
+
+const AuthenticationPage = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Tabs defaultValue="login">
+          <TabsList className="w-full">
+            <TabsTrigger value="login">Entrar</TabsTrigger>
+            <TabsTrigger value="register">Criar conta</TabsTrigger>
+          </TabsList>
+          <TabsContent value="login">
+            <LoginForm />
+          </TabsContent>
+          <TabsContent value="register">
+            <SignUpForm />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+
+export default AuthenticationPage;
